89bc85fbc2561dff202c70ea88e3b889dff524a3
D34ckgler <herogamers.ortiz@gmail.com>
2021-05-19 16:16:54 -0400

    Nuevo verificador

---
+++ src/public_html/assets/js/verificador.js
@@ -0,0 +1,125 @@
+var szDesc = document.getElementById("szDescription");
+var prcbs = document.getElementById("prcbs");
+var prcusd = document.getElementById("prcusd");
+var ntasa = document.getElementById("ntasa");
+var publishing = document.getElementById("publishing");
+var isprice = document.getElementById("isprice");
+
+setTimeout(function() {
+    document.location.reload();
+}, 200000)
+
+var codigo = "";
+$(document).on('keypress', function(e) {
+    if (e.key >= 0 && e.key < 9) {
+        //codigo = codigo + e.key;
+    }
+    if (e.which == 13) {
+        $(isprice).fadeIn('fast');
+        $(publishing).fadeOut('fast');
+        //szCode.value = codigo;
+        //Toggle fullscreen off, activate it
+        if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            } else if (document.documentElement.mozRequestFullScreen) {
+                document.documentElement.mozRequestFullScreen(); // Firefox
+            } else if (document.documentElement.webkitRequestFullscreen) {
+                document.documentElement.webkitRequestFullscreen(); // Chrome and Safari
+            } else if (document.documentElement.msRequestFullscreen) {
+                document.documentElement.msRequestFullscreen(); // IE
+            }
+
+            //Toggle fullscreen on, exit fullscreen
+        }
+        /* else {
+               
+                           if (document.exitFullscreen) {
+                               document.exitFullscreen();
+                           } else if (document.msExitFullscreen) {
+                               document.msExitFullscreen();
+                           } else if (document.mozCancelFullScreen) {
+                               document.mozCancelFullScreen();
+                           } else if (document.webkitExitFullscreen) {
+                               document.webkitExitFullscreen();
+                           }
+                       }*/
+        /*if (szCode.value == "exit") {
+            window.close();
+        }*/
+        ScanBar(codigo);
+    }
+});
+
+$.ajaxSetup({
+    // Disable caching of AJAX responses
+    cache: false
+});
+
+$(document).keypress(function(event) {
+    var letter = event.which || event.keyCode;
+    //alert('Handler for .keypress() called. - ' + String.fromCharCode(letter));
+    codigo = codigo + String.fromCharCode(letter);
+    console.info(String.fromCharCode(letter))
+});
+
+var flag = false;
+
+function ScanBar(sku) {
+    codigo = '';
+    if (flag) return;
+
+    $.ajax({
+        url: "http://10.10.100.104/jsonp/" + sku + "/10.10.10.10",
+        context: document.body,
+        statusCode: {
+            404: function() {
+                szDesc.value = "Producto no encontrado";
+                setTimeout(function() {
+                    $(isprice).fadeOut('fast');
+                    $(publishing).fadeIn('fast');
+                    szDesc.value = "";
+                }, 2000)
+            }
+        },
+        success: function(data, i) {
+            flag = true;
+            szDesc.value = data[0].C_DESCRI;
+            prcbs.value = data[0].format;
+            ntasa.value = data[0]['tasaf'];
+            prcusd.value = data[0].prcusd;
+            //szCode.value = "";
+            setTimeout(function() {
+                $(publishing).show('fast');
+                codigo = "";
+                szDesc.value = "";
+                prcbs.value = '';
+                ntasa.value = '';
+                prcusd.value = '';
+                flag = false
+            }, 3000);
+        }
+    }).done(function(data) {
+        //if (data == undefined || data == null) return szDesc.value = "Producto no existe en Inventario";
+        /*flag = true;
+        szDesc.value = data[0].C_DESCRI
+        prcbs.value = data[0].format
+        ntasa.value = data[0]['tasaf']
+        prcusd.value = data[0].prcusd
+
+        setTimeout(function () {
+            //szCode.removeAttribute('disabled');
+            szCode.value = "";
+            codigo = "";
+            szDesc.value = "";
+            prcbs.value = '';
+            ntasa.value = '';
+            prcusd.value = '';
+            flag = false
+        }, 7000);*/
+        data = null;
+    });
+}
+
+//szCode.focus();
+$('body').css("background-size", "cover");
