6e14fa3e7943446dc6e2ad11eb34fa1769ceba2f
d34ckgler <herogamers.ortiz@gmail.com>
2023-08-04 16:07:13 -0400

    Ajuste format decimal

--- src/functions.ts
+++ src/functions.ts
@@ -1,10 +1,11 @@
 export function format(n, sep, decimals) {
     sep = sep || "."; // Default to period as decimal separator
     decimals = decimals || 2; // Default to 2 decimals
+    
 
     return n.toLocaleString().split(sep)[0]
-        + sep
-        + n.toFixed(decimals).split(sep)[1];
+        + sep.slice(1)
+        // + n.toFixed(decimals).split(sep)[1];
 }
 
 export function getOrg(org) {
